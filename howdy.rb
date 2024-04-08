f968867cb98f1a4aee701ad7424fb2beb6f65357
drdpatel <165212603+drdpatel@users.noreply.github.com>
2024-04-08 21:28:32 +0000

    end of monday

--- howdy.rb
+++ howdy.rb
@@ -1,3 +1,15 @@
 my_string = "hello, world"
 pp my_string
 
+require"./goodbye.rb"
+require"active_support/all"
+
+pp "What's your name?"
+
+their_name = gets
+pp "Hello, " + their_name + "!"
+puts "Hello, " + their_name + "!"
+
+their_name = gets.chomp
+pp "Hello, " + their_name + "!"
+puts "Hello, " + their_name + "!"
